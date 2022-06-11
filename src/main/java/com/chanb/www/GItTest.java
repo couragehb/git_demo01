4ce240b700d528a286472c3921674515df0f9c0c
Chanb <3339338696@qq.com>
2022-06-11 20:22:56 +0800

    firstCommit

---
+++ src/main/java/com/chanb/www/GItTest.java
@@ -0,0 +1,14 @@
+package com.chanb.www;
+
+/**
+ * @ClassName GItTest
+ * @Description TODO
+ * @Author cheng
+ * @Date 2022/6/11 20:18
+ * @Version
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("Git Test");
+    }
+}
